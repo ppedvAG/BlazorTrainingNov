644ab049c7461e0df716ff64b8631cd51a2a0e12
Hannes Preishuber <hannesp@ppedv.de>
2022-11-07 11:50:33 +0100

    HTML Blazor

---
+++ BlazorTrainingNov/Pages/Modul2/demo4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingNov.Pages.Modul2
+{
+    public class demo4Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
