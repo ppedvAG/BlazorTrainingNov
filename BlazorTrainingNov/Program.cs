43094f7414c73789f2eaf7b55dd9977cdb2ea85d
Hannes Preishuber <hannesp@ppedv.de>
2022-11-07 11:31:37 +0100

    Labs PDF

--- BlazorTrainingNov/Program.cs
+++ BlazorTrainingNov/Program.cs
@@ -14,6 +14,7 @@ namespace BlazorTrainingNov
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            
 
             var app = builder.Build();
 
