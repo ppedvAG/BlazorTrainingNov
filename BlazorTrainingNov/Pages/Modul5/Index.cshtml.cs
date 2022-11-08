ccdb69a2b02d2fdd90d0803595fcf426c9cf6864
Hannes Preishuber <hannesp@ppedv.de>
2022-11-08 10:21:37 +0100

    Suche mit Enter Taste auslösen

---
+++ BlazorTrainingNov/Pages/Modul5/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingNov.Pages.Modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
