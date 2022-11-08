e6530f2e39501191b7b78df81f74d2ea5e95a32f
Hannes Preishuber <hannesp@ppedv.de>
2022-11-08 11:12:25 +0100

    Editform mit Model und Validierung per Code

---
+++ BlazorTrainingNov/Pages/Modul5/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingNov.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,ErrorMessage ="max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(140,190,ErrorMessage ="zwischne 140 und 190")]
+        public int Gross { get; set; }
+
+    }
+}
