f77412e40cc07cf88f4eb700b8e415f40981054d
Hannes Preishuber <hannesp@ppedv.de>
2022-11-08 11:21:54 +0100

    FIX Bootstrap CSS validierung per Provider

---
+++ BlazorTrainingNov/Pages/Modul5/BootstrapFixFieldCssClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTrainingNov.Pages.Modul5
+{
+    public class BootstrapFixFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isinvalid = editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isinvalid?"is-invalid":"is-valid";
+            // Blazor defautl invalid valid
+        }
+    }
+}
